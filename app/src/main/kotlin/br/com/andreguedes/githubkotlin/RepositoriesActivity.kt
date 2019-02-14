3b116db5ea5f04aa037ae17ddc1e5503ed7fbeed
andreguedes <andresguedes@live.com>
2019-02-14 15:43:05 -0200

    Adding Fabric.io

--- app/src/main/kotlin/br/com/andreguedes/githubkotlin/RepositoriesActivity.kt
+++ app/src/main/kotlin/br/com/andreguedes/githubkotlin/RepositoriesActivity.kt
@@ -2,12 +2,16 @@ package br.com.andreguedes.githubkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 
 class RepositoriesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_repositories)
+
+        Fabric.with(this, Crashlytics())
     }
 
 }
